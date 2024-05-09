384a04bcf43b6c4a32ff2a66498bf3c9fbdea3b7
Duvidu kavinda <152588529+duvidu@users.noreply.github.com>
2024-05-09 11:46:59 +0530

    final changes

--- Frontend/src/Components/Leave/Navibar.jsx
+++ Frontend/src/Components/Leave/Navibar.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom'
+import leaveCss from './leave.module.css';
+
 
 const Navibar =  () => {
     return(
         <header>
             <div className="#">
                 <Link to="/">
-                    <h1 className="LevDet">Laeve details</h1>
+                    <h1 className={leaveCss.LevDet}>Laeve details</h1>
                 </Link>
                 
             </div>
